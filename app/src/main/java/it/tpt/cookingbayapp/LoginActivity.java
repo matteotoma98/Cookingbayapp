36833737d008f87f26ae302d11332dee96dd3595
ABTCoder <amalbenson98@gmail.com>
2020-03-25 17:21:14 +0100

    Database instance + preferences 2

--- app/src/main/java/it/tpt/cookingbayapp/LoginActivity.java
+++ app/src/main/java/it/tpt/cookingbayapp/LoginActivity.java
@@ -22,7 +22,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //Istanza database
+        //Istanza database firebase
         mAuth = FirebaseAuth.getInstance();
 
         textEmail = findViewById(R.id.textEmail);
