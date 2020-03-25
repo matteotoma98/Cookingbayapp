5c4c119b54a9a44b994970248980fc9c2c0ee4d2
matteotoma98 <matteotoma98@hotmail.it>
2020-03-25 15:56:46 +0100

    Aggiunti metodi

--- app/src/main/java/com/example/cookingbayapp/MainActivity.java
+++ app/src/main/java/com/example/cookingbayapp/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.cookingbayapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int LOGIN_REQUEST = 101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class); //pushiamo la login activity
+        startActivityForResult(intent, LOGIN_REQUEST);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if(requestCode == LOGIN_REQUEST) {
+            if(resultCode == RESULT_OK) {
+                String nome = intent.getExtras().getString("nome");
+                String cognome = intent.getExtras().getString("cognome");
+
+                getSupportActionBar().setTitle(nome + " " + cognome); //dà il nome e il cognome in alto nella action bar
+            }
+        }
+    }
+
+
+
+
+
+
 }
