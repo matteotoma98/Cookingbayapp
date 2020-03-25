a0174ac68232c44d7bf0eb41a0da55d1209d74b2
Federico <federico.panackal@outlook.com>
2020-03-25 17:13:32 +0100

    Modifica da login a registrazione

--- app/src/main/java/it/tpt/cookingbayapp/LoginActivity.java
+++ app/src/main/java/it/tpt/cookingbayapp/LoginActivity.java
@@ -11,20 +11,23 @@ import it.tpt.cookingbayapp.R;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
-    private TextInputEditText textEmail, textPassword;
+    private TextInputEditText textName, textSurname, textEmail, textPassword;
     private Button btnRegistra;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        textEmail = (TextInputEditText)findViewById(R.id.textEmail);
-        textPassword = (TextInputEditText)findViewById(R.id.textPassword);
+        textName = findViewById(R.id.textName);
+        textSurname = findViewById(R.id.textSurname);
+        textEmail = findViewById(R.id.textEmail);
+        textPassword = findViewById(R.id.textPassword);
         btnRegistra = findViewById(R.id.containedButton);
         btnRegistra.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
+                intent.putExtra("name", textName.getText().toString());
+                intent.putExtra("surname", textSurname.getText().toString());
                 intent.putExtra("email", textEmail.getText().toString()); //il to string nella edit text è necessario altrimenti get text non restituisce una stringa
                intent.putExtra("password", textPassword.getText().toString());
 
