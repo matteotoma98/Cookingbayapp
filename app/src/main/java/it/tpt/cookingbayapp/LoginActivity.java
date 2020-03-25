02f7241a6c218044d2ecb983617d176153bfcd3f
matteotoma98 <matteotoma98@hotmail.it>
2020-03-25 16:42:42 +0100

    Rinominati packages

--- app/src/main/java/it/tpt/cookingbayapp/LoginActivity.java
+++ app/src/main/java/it/tpt/cookingbayapp/LoginActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.cookingbayapp.R;
+import it.tpt.cookingbayapp.R;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
