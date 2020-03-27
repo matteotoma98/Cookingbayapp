a04855f08025a4e3e1d03151746fddab1f0ebcd6
matteotoma98 <matteotoma98@hotmail.it>
2020-03-27 19:39:56 +0100

    Aggiunta Cardview

--- app/src/main/java/it/tpt/cookingbayapp/CreateRecipe.java
+++ app/src/main/java/it/tpt/cookingbayapp/CreateRecipe.java
@@ -3,13 +3,26 @@ package it.tpt.cookingbayapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class CreateRecipe extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_recipe);
+        getSupportActionBar().setTitle("Crea nuova ricetta");
+        getSupportActionBar().setIcon(R.drawable.maxresdefault);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
     }
 }
