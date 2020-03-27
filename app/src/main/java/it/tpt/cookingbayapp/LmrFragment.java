e240ebed29146a747fda8375f05517f96e291db8
matteotoma98 <matteotoma98@hotmail.it>
2020-03-27 18:00:37 +0100

    Aggiunta Cardview

--- app/src/main/java/it/tpt/cookingbayapp/LmrFragment.java
+++ app/src/main/java/it/tpt/cookingbayapp/LmrFragment.java
@@ -1,19 +1,55 @@
 package it.tpt.cookingbayapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class LmrFragment extends Fragment {
 
+    private FloatingActionButton btnCrea;
     @Nullable
     @Override
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_lmr,container,false);
+
+
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnCrea = (FloatingActionButton)getView().findViewById(R.id.floating_action_button);
+        btnCrea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(getActivity(),CreateRecipe.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+
+
+
+
+
+
 }
