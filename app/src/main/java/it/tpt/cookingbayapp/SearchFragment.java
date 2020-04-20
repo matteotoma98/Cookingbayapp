4d71c9f2eae0dfa7e1ef44edd83808d1ee3955cd
ABTCoder <amalbenson98@gmail.com>
2020-04-20 17:00:48 +0200

    IngNames adapter update 5

--- app/src/main/java/it/tpt/cookingbayapp/SearchFragment.java
+++ app/src/main/java/it/tpt/cookingbayapp/SearchFragment.java
@@ -7,40 +7,69 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.chip.ChipGroup;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 import java.util.ArrayList;
 
+import it.tpt.cookingbayapp.cardRecycler.RecipeCardRecyclerViewAdapter;
 import it.tpt.cookingbayapp.ingNamesRecyler.IngNamesAdapter;
 
-public class SearchFragment extends Fragment implements View.OnClickListener{
+public class SearchFragment extends Fragment implements View.OnClickListener {
 
-    private RecyclerView ingRecyclerView;
+    private RecyclerView ingRecyclerView, recipeRecyclerView;
+    private RecipeCardRecyclerViewAdapter recipeAdapter;
     private IngNamesAdapter ingAdapter;
     private TextInputEditText ingName;
     private ImageView addIng, delIng;
+    private ChipGroup chipGroup;
+    private Button search;
+    private FirebaseFirestore db;
+    private CollectionReference recipes;
+    private String selectedChip;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_search,container,false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
 
         ingName = view.findViewById(R.id.txtIngNameSearch);
         addIng = view.findViewById(R.id.addIngName);
         delIng = view.findViewById(R.id.delIngName);
+        search = view.findViewById(R.id.btnSearch);
+        chipGroup = view.findViewById(R.id.chip_group);
+        selectedChip = "";
+
+
+        recipeRecyclerView = view.findViewById(R.id.searchRecycler);
+        recipeRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1, GridLayoutManager.VERTICAL, false));
 
         ingRecyclerView = view.findViewById(R.id.ingNameRecycler);
         ingRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         ingAdapter = new IngNamesAdapter(new ArrayList<String>());
         ingRecyclerView.setAdapter(ingAdapter);
 
+        db = FirebaseFirestore.getInstance();
+        recipes = db.collection("Recipes");
+
+
         return view;
+    }
+
+    private Query generateQuery() {
+        Query query;
+        if()
     }
 
     @Override
@@ -48,6 +77,24 @@ public class SearchFragment extends Fragment implements View.OnClickListener{
         super.onViewCreated(view, savedInstanceState);
         addIng.setOnClickListener(this);
         delIng.setOnClickListener(this);
+        search.setOnClickListener(this);
+        chipGroup.setOnCheckedChangeListener(new ChipGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(ChipGroup group, @IdRes int checkedId) {
+                switch (group.getCheckedChipId()) {
+                    case
+                    case
+                    case
+                    case
+                    case
+                    case
+                    case
+                    default:
+                        selectedChip = "";
+                        break;
+                }
+            }
+        });
     }
 
     @Override
@@ -55,12 +102,15 @@ public class SearchFragment extends Fragment implements View.OnClickListener{
         switch (v.getId()) {
             case R.id.addIngName:
                 String ing = ingName.getText().toString().trim();
-                if(!ing.equals("")) {
+                if (!ing.equals("")) {
                     ingAdapter.addIngredient(ing.toLowerCase());
                 }
                 break;
             case R.id.delIngName:
                 ingAdapter.delIngredient();
+                break;
+            case R.id.btnSearch:
+
                 break;
         }
     }
