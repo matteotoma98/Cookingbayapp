4103f8d941f3aec21063e40aeb7f78d12ea9c909
ABTCoder <amalbenson98@gmail.com>
2020-04-20 16:32:49 +0200

    IngNames adapter update 3

--- app/src/main/java/it/tpt/cookingbayapp/SearchFragment.java
+++ app/src/main/java/it/tpt/cookingbayapp/SearchFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,23 +19,49 @@ import java.util.ArrayList;
 
 import it.tpt.cookingbayapp.ingNamesRecyler.IngNamesAdapter;
 
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment implements View.OnClickListener{
 
     private RecyclerView ingRecyclerView;
     private IngNamesAdapter ingAdapter;
     private TextInputEditText ingName;
+    private ImageView addIng, delIng;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search,container,false);
 
-        ingName = view.findViewById(R.id.ingNameTxtView);
+        ingName = view.findViewById(R.id.txtIngNameSearch);
+        addIng = view.findViewById(R.id.addIngName);
+        delIng = view.findViewById(R.id.delIngName);
 
         ingRecyclerView = view.findViewById(R.id.ingNameRecycler);
         ingRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         ingAdapter = new IngNamesAdapter(new ArrayList<String>());
+        ingRecyclerView.setAdapter(ingAdapter);
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        addIng.setOnClickListener(this);
+        delIng.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.addIngName:
+                String ing = ingName.getText().toString().trim();
+                if(!ing.equals("")) {
+                    ingAdapter.addIngredient(ing.toLowerCase());
+                }
+                break;
+            case R.id.delIngName:
+                ingAdapter.delIngredient();
+                break;
+        }
     }
 }
