e28c7a5174ec0554658525c0d02e5da884fc032c
ABTCoder <amalbenson98@gmail.com>
2020-04-20 15:59:55 +0200

    IngNames adapter

---
+++ app/src/main/java/it/tpt/cookingbayapp/ingNamesRecyler/IngNamesAdapter.java
@@ -0,0 +1,56 @@
+package it.tpt.cookingbayapp.ingNamesRecyler;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import it.tpt.cookingbayapp.R;
+
+public class IngNamesAdapter extends RecyclerView.Adapter<IngNamesViewHolder> {
+
+    private ArrayList<String> ingredients;
+
+    public IngNamesAdapter(ArrayList<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    @NonNull
+    @Override
+    public IngNamesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.ingredient_layout, parent, false);
+        return new IngNamesViewHolder(layoutView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull IngNamesViewHolder holder, int position) {
+        if (ingredients != null && position < ingredients.size()) {
+            holder.ingredient.setText(ingredients.get(position));
+        }
+    }
+
+    public void addIngredient(String ing) {
+        ingredients.add(ing.trim().toLowerCase());
+        notifyItemInserted(ingredients.size() - 1);
+    }
+
+    public void delIngredient() {
+        if (getItemCount() != 0) {
+            ingredients.remove(ingredients.size() - 1);
+            notifyItemRemoved(ingredients.size());
+        }
+    }
+
+    public ArrayList<String> getIngredients() {
+        return ingredients;
+    }
+
+    @Override
+    public int getItemCount() {
+        return ingredients.size();
+    }
+}
---
+++ app/src/main/java/it/tpt/cookingbayapp/ingNamesRecyler/IngNamesViewHolder.java
@@ -0,0 +1,20 @@
+package it.tpt.cookingbayapp.ingNamesRecyler;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import it.tpt.cookingbayapp.R;
+
+public class IngNamesViewHolder extends RecyclerView.ViewHolder {
+
+    TextView ingredient;
+
+    public IngNamesViewHolder(@NonNull View itemView) {
+        super(itemView);
+        ingredient = itemView.findViewById(R.id.ingredient);
+    }
+
+}
