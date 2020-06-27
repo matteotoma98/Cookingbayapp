ef992fad7fd450cf52b713a14795e5c3f1a93efe
Matteo Toma <matteotoma98@hotmail.it>
2020-06-27 12:50:36 +0200

    Controllo connessione ad Internet

--- app/src/main/java/it/tpt/cookingbayapp/LoginActivity.java
+++ app/src/main/java/it/tpt/cookingbayapp/LoginActivity.java
@@ -1,7 +1,10 @@
 package it.tpt.cookingbayapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -68,11 +71,17 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
         }
     }
-
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.loginButton: //Bottone accedi
+                if(isNetworkAvailable()==true){
                 try {
                     mAuth.signInWithEmailAndPassword(textEmail.getText().toString().trim(), textPassword.getText().toString())
                             .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -94,7 +103,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             });
                 } catch (NullPointerException e) {
                     Toast.makeText(LoginActivity.this, getString(R.string.required), Toast.LENGTH_LONG).show();
-                }
+                } } else   Toast.makeText(LoginActivity.this, "Non sei connesso ad Internet", Toast.LENGTH_LONG).show();
                 break;
 
             case R.id.register: //Passa a registrati
